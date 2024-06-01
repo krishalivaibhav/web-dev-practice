126169338dba422faa6ee6c42363e25963439cbf
krishalivaibhav <158697719+krishalivaibhav@users.noreply.github.com>
2024-06-02 01:20:33 +0530

    Add files via upload

---
+++ back-end/2.2 Native Modules/index.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+/*fs.writeFile("message.txt", "hello form nodeJs", (err) =>{
+    if (err) throw err;
+    console.log("the file has been saved!");
+});*/
+fs.readFile("./message.txt", "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
