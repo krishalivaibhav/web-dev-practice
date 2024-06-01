deb16601a933758f98eb91b1c6b12f3aea8a8a52
krishalivaibhav <158697719+krishalivaibhav@users.noreply.github.com>
2024-06-02 01:17:51 +0530

    Delete 2.2 Native Modules directory

--- 2.2 Native Modules/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-/*fs.writeFile("message.txt", "hello form nodeJs", (err) =>{
-    if (err) throw err;
-    console.log("the file has been saved!");
-});*/
-fs.readFile("./message.txt", "utf8", (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
